c7d89e35a888115084fc42815cd4afec25b8f11e
Jacek Kuźmicz <jkuzmicz@psi.de>
2023-05-05 22:20:35 +0200

    mods

---
+++ Common/Log/FileLogWriter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SystemKadr.Common.Interface;
+
+namespace SystemKadr.Common.Log
+{
+    public class FileLogWriter : ILogWriter
+    {
+        private object _lock = new object();
+        private string fileNameAndPath;
+
+        public FileLogWriter(string fileNameAndPath)
+        {
+            this.fileNameAndPath = fileNameAndPath;
+        }
+
+        public void Write(string message)
+        {
+            lock (_lock)
+            {
+                File.AppendAllText(fileNameAndPath, message+ "\n");
+            }
+        }
+    }
+}
